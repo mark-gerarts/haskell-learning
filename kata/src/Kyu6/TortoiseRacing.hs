ae51883496ee47c9f191590c468fce42d5649bdd
Mark Gerarts <mark.gerarts@gmail.com>
2018-09-10 12:05:21 +0200

    Solve tortoise racing

--- kata/src/Kyu6/TortoiseRacing.hs
+++ kata/src/Kyu6/TortoiseRacing.hs
@@ -3,15 +3,16 @@ module Kyu6.TortoiseRacing where
 -- https://www.codewars.com/kata/55e2adece53b4cdcb900006c/train/haskell
 race :: Int -> Int -> Int -> Maybe (Int, Int, Int)
 race v1 v2 g
-  | v2 < v1 = Nothing
+  | v2 <= v1 = Nothing
   | otherwise = Just (parseSeconds $ raceInSeconds v1 v2 g)
 
 raceInSeconds :: Int -> Int -> Int -> Int
-raceInSeconds v1 v2 g = g `quot` (v2 - v1)
+raceInSeconds v1 v2 g = floor $ (* 3600) hours
+  where
+    hours = (fromIntegral g :: Double) / fromIntegral (v2 - v1)
 
 parseSeconds :: Int -> (Int, Int, Int)
-parseSeconds totalSeconds = (hours, minutes, seconds)
+parseSeconds totalSeconds = (h, m, s)
   where
-    hours = totalSeconds `div` 3600
-    minutes = (totalSeconds - (hours * 3600)) `div` 60
-    seconds = totalSeconds - (hours * 3600) - (minutes * 60)
+    (h, s1) = totalSeconds `divMod` 3600
+    (m, s) = s1 `divMod` 60
--- kata/test/Kyu6/TortoiseRacingSpec.hs
+++ kata/test/Kyu6/TortoiseRacingSpec.hs
@@ -9,9 +9,19 @@ testTortoise v1 v2 g s =
   it (printf "should return race where v1: %d v2: %d lead: %d " v1 v2 g) $
     race v1 v2 g `shouldBe` s
 
+testSeconds :: Int -> (Int, Int, Int) -> Spec
+testSeconds s expected =
+  it (printf "should parse seconds with %d seconds" s) $
+    parseSeconds s `shouldBe` expected
+
 spec :: Spec
 spec =
     describe "Basic Tests" $ do
         testTortoise 720 850 70 (Just (0, 32, 18))
         testTortoise 80 100 40 (Just (2, 0, 0))
         testTortoise 80 91 37 (Just (3, 21, 49))
+        testTortoise 720 850 37 (Just (0, 17, 4))
+        testSeconds 3600 (1, 0, 0)
+        testSeconds 3601 (1, 0, 1)
+        testSeconds 72 (0, 1, 12)
+        testSeconds 3599 (0, 59, 59)
