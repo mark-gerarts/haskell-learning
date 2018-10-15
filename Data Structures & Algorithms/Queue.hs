db542478ca8c713ec27194887f4ce9e668d9d0fc
Mark Gerarts <mark.gerarts@gmail.com>
2018-10-15 22:06:35 +0200

    Implement simple queue

---
+++ Data Structures & Algorithms/Queue.hs
@@ -0,0 +1,23 @@
+module Queue where
+
+import Prelude
+
+type Queue a = [a]
+
+enqueue :: a -> Queue a -> Queue a
+enqueue x q = x : q
+
+dequeue :: Queue a -> (Maybe a, Queue a)
+dequeue [] = (Nothing, [])
+dequeue q = (Just (head q), tail q)
+
+first :: Queue a -> Maybe a
+first [] = Nothing
+first q = Just (head q)
+
+isEmpty :: Queue a -> Bool
+isEmpty [] = True
+isEmpty _ = False
+
+size :: Queue a -> Int
+size = length
