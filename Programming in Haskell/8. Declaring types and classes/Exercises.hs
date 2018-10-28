6e49e235331f62f7149a767b09825b36e0ea53e7
Mark Gerarts <mark.gerarts@gmail.com>
2018-10-28 21:25:19 +0100

    Continue exercises chapter 8

--- Programming in Haskell/8. Declaring types and classes/Exercises.hs
+++ Programming in Haskell/8. Declaring types and classes/Exercises.hs
@@ -47,3 +47,46 @@ numLeaves (Node' left right) = numLeaves left + numLeaves right
 isBalanced :: Tree' a -> Bool
 isBalanced (Leaf' _) = True
 isBalanced (Node' left right) = abs (numLeaves left - numLeaves right) <= 1
+
+-- 4. Define a function `balance`, which converts a list into a balanced tree.
+split :: [a] -> ([a], [a])
+split xs = (take l xs, drop l xs)
+  where
+    l = length xs `div` 2
+
+balance :: [a] -> Tree' a
+balance [x] = Leaf' x
+balance xs = Node' (balance left) (balance right)
+  where
+    (left, right) = split xs
+
+-- 5. Define `folde`
+data Expr = Val Int | Add Expr Expr
+
+folde :: (Int -> a) -> (a -> a -> a) -> Expr -> a
+folde f _ (Val x) = f x
+folde f g (Add x y) = g (folde f g x) (folde f g y)
+
+-- 6. Define an `eval` function using `folde`, as well as a `size` function,
+--    which calculates the number of values.
+eval :: Expr -> Int
+eval = folde id (+)
+
+size :: Expr -> Int
+size = folde (const 1) (+)
+
+-- 7. Complete the following instance declarations.
+-- instance Eq a => Eq (Maybe a) where
+--   Nothing == Nothing = True
+--   Just x == Just y = x == y
+--   _ == _ = False
+
+-- instance Eq a => Eq [a] where
+--   [] == [] = True
+--   (x:xs) == (y:ys) = x == y && xs == ys
+
+-- 8. Extend the tautology checker to suppor disjuntion and equivalence.
+-- See TautologyChecker.hs
+
+-- 9. Extend the abstract machine to support multiplication
+-- See AbstractMachine.hs
