9ab495c8bf9bb17c94e7093baf6f94a56d3d6526
Mark Gerarts <mark.gerarts@gmail.com>
2019-02-27 20:30:52 +0100

    Implement random word

--- Projects/hangman/src/Hangman.hs
+++ Projects/hangman/src/Hangman.hs
@@ -1,1 +1,30 @@
 module Hangman where
+
+import System.Random
+import Data.Char
+
+data Game = Game { _targetWord :: String
+                 , _guesses :: [Char]
+                 , _lives :: Int
+                 } deriving (Show)
+
+loadWords :: IO [String]
+loadWords = do
+  wordFile <- readFile "data/words.txt"
+  return (lines wordFile)
+
+randomWord :: IO String
+randomWord = do
+  words <- loadWords
+  r <- randomRIO (0, (length words) - 1)
+  return (words !! r)
+
+guessLetter :: Char -> Game -> Game
+guessLetter c (Game targetWord guesses lives) =
+  Game targetWord (c : guesses) newLives
+  where
+    newLives = if containsLetter c targetWord then lives else lives - 1
+
+
+containsLetter :: Char -> String -> Bool
+containsLetter c = (c `elem`) . map toLower
--- Projects/hangman/src/UI.hs
+++ Projects/hangman/src/UI.hs
@@ -1,15 +1,3 @@
 module UI where
 
-import System.Random
-
 import Hangman
-
-data Game = Game { targetWord :: String
-                 , guesses :: [Char]
-                 , lives :: Int
-                 }
-
-loadWords :: IO [String]
-loadWords = do
-  wordList <- readFile "data/words.txt"
-  return (lines wordList)
